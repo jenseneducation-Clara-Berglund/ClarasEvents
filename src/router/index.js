4b2ee894524e1eb0437487d4a82daea99bd8e51f
jenseneducation-Clara-Berglund <54704837+jenseneducation-Clara-Berglund@users.noreply.github.com>
2020-09-24 21:23:46 +0200

    clean up autogenerated code

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import Vue from "vue"
-import VueRouter from "vue-router"
-import Home from "../views/Home.vue"
-import Profile from "../views/Profile.vue"
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Profile from "../views/Profile.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -16,22 +16,13 @@ const routes = [
     path: "/Profile",
     name: "Profile",
     component: Profile
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
-]
+];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
