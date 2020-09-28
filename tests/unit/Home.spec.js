b267e51ae5210491f08bad69f063d1caf2d68cfa
jenseneducation-Clara-Berglund <54704837+jenseneducation-Clara-Berglund@users.noreply.github.com>
2020-09-28 10:47:03 +0200

    autogenerated formatfix

--- tests/unit/Home.spec.js
+++ tests/unit/Home.spec.js
@@ -1,15 +1,15 @@
-import Home from "@/views/Home.vue";
-import Button from "@/components/Button.vue";
-import store from "@/store/index.js";
-const { shallowMount, createLocalVue } = require("@vue/test-utils");
+import Home from "@/views/Home.vue"
+import Button from "@/components/Button.vue"
+import store from "@/store/index.js"
+const { shallowMount, createLocalVue } = require("@vue/test-utils")
 
 it("should be able to see the myProfileButton component at the home view", () => {
-  const localVue = createLocalVue();
+  const localVue = createLocalVue()
   const wrapper = shallowMount(Home, {
     localVue,
     store
-  });
+  })
 
-  const actual = wrapper.findComponent(Button).exists();
-  expect(actual).toBe(true);
-});
+  const actual = wrapper.findComponent(Button).exists()
+  expect(actual).toBe(true)
+})
