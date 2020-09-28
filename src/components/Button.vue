950b65b54bb5bfe946181a714c2268700342cd5c
jenseneducation-Clara-Berglund <54704837+jenseneducation-Clara-Berglund@users.noreply.github.com>
2020-09-28 20:59:31 +0200

    tests for button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,15 +1,25 @@
 <template>
-  <div class="ButtonContainer">
-    <button>{{ title }}</button>
+  <div class="buttonContainer" v-on:click="$emit('clicked')">
+    {{ title }}
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    title: String
-  }
-}
+    title: String,
+  },
+};
 </script>
 
-<style></style>
+<style>
+.buttonContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: lightpink;
+  height: 4em;
+  min-width: 3em;
+  max-width: 9em;
+}
+</style>
