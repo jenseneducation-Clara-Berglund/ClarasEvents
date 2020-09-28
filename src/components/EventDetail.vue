ccccc837ec6fe7e9726c57018449e8ac0fb06445
jenseneducation-Clara-Berglund <54704837+jenseneducation-Clara-Berglund@users.noreply.github.com>
2020-09-28 16:48:08 +0200

    add test for EventDetail-view

--- src/components/EventDetail.vue
+++ src/components/EventDetail.vue
@@ -5,20 +5,35 @@
         class="eventImgContainer"
         :src="require('@/assets/' + event.image)"
       />
+      <CloseButton @clicked="$emit('close')" />
       <div class="eventDateContainer">{{ event.date }}</div>
       <div class="eventInfoContainer">{{ event.info }}</div>
-      <CloseButton />
-      <Button title="Join Event" />
     </div>
+    <Button title="Join Event" />
   </div>
 </template>
 
 <script>
+import CloseButton from "@/components/CloseButton.vue";
 export default {
   props: {
-    event: Object
-  }
-}
+    event: Object,
+  },
+  components: {
+    CloseButton,
+  },
+};
 </script>
 
-<style></style>
+<style>
+.eventDetailContainer {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
--- tests/unit/EventDetail.spec.js
+++ tests/unit/EventDetail.spec.js
@@ -1,28 +1,34 @@
-import Home from "@/views/Home.vue";
-import EventDetail from "@/components/EventDetail.vue";
-import store from "@/store/index.js";
-const { shallowMount, createLocalVue } = require("@vue/test-utils");
+import EventDetail from "@/components/EventDetail.vue"
+import store from "@/store/index.js"
+import CloseButton from "@/components/CloseButton.vue"
+
+const { shallowMount, createLocalVue } = require("@vue/test-utils")
 
 describe("EventDetailTests", () => {
-  it("should be able to se the event info at the EventDetail component", () => {
-    const wrapper = shallowMount(EventDetail, {
-      localVue,
-      store,
-      propsData: {
-        event: {
-          id: 1,
-          name: "We love code",
-          date: "17 Oct",
-          info:
-            "Meetup for coders! Join this event to meet new code-loving friends!",
-          image: "codeEvent.jpg",
-        },
-      },
-    });
+  const localVue = createLocalVue()
+  const wrapper = shallowMount(EventDetail, {
+    localVue,
+    store,
 
+    propsData: {
+      event: {
+        id: 1,
+        name: "We love code",
+        date: "17 Oct",
+        info:
+          "Meetup for coders! Join this event to meet new code-loving friends!",
+        image: "codeEvent.jpg"
+      }
+    }
+  })
+  it("should be able to se the event info at the EventDetail component", () => {
     const expected =
-      "Meetup for coders! Join this event to meet new code-loving friends!";
-    const actual = wrapper.find(".eventInfoContainer").text();
-    expect(actual).toBe(expected);
-  });
-});
+      "Meetup for coders! Join this event to meet new code-loving friends!"
+    const actual = wrapper.find(".eventInfoContainer").text()
+    expect(actual).toBe(expected)
+  })
+  it("should be able to see the CloseButton component at the EventDetail view", () => {
+    const actual = wrapper.findComponent(CloseButton).exists()
+    expect(actual).toBe(true)
+  })
+})
