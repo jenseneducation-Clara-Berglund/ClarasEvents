4a3027766b6107ba60abc1d2a91c3adace04f19a
jenseneducation-Clara-Berglund <54704837+jenseneducation-Clara-Berglund@users.noreply.github.com>
2020-09-30 13:23:26 +0200

    add test for JoinEvent view + add JoinEvent view

---
+++ src/views/JoinEvent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="joinEventContainer">
+    <Form />
+  </div>
+</template>
+
+<script>
+import EventItem from "@/components/EventItem.vue";
+import EventDetail from "@/components/EventDetail.vue";
+import Form from "@/components/Form.vue";
+
+export default {
+  components: {
+    EventItem,
+    EventDetail,
+    Form,
+  },
+};
+</script>
+
+<style>
+</style>
