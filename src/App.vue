5892b235fce0bf57c358591b434924131b0d7060
jenseneducation-Clara-Berglund <54704837+jenseneducation-Clara-Berglund@users.noreply.github.com>
2020-09-30 22:18:28 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,21 @@
 
 <style lang="scss">
 #app {
+  display: flex;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+}
+body,
+html {
+  width: 100%;
+  height: 100%;
+  margin: 0;
 }
 
 #nav {
