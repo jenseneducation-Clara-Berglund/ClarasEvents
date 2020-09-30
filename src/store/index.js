356bdca67e6518b7ab888cb7f9bfc8cd78af0f6d
jenseneducation-Clara-Berglund <54704837+jenseneducation-Clara-Berglund@users.noreply.github.com>
2020-09-30 08:46:10 +0200

    update state + create state for attended events

--- src/store/index.js
+++ src/store/index.js
@@ -8,25 +8,53 @@ export default new Vuex.Store({
     listOfEvents: [
       {
         id: 1,
-        name: "We love code",
+        name: "Meetup with your pet rabbit",
+        date: "1 Oct",
+        info:
+          "Hop toghether with other pet-rabbits and their owners! Carrots are free of charge!",
+        image: "bunny-1149060_640.jpg"
+      },
+
+      {
+        id: 2,
+        name: "Cook Dinner",
+        date: "14 Nov",
+        info:
+          "Cook toghether in a restaurant-kitchen and be led by the famous chef Clarissa Tomato",
+        image: "kitchen-731351_640.jpg"
+      },
+
+      {
+        id: 3,
+        name: "Shop til´you drop",
+        date: " 28 Oct",
+        info:
+          "Shop toghether with personal stylists",
+        image: "woman-3040029_640.jpg"
+      },
+
+      {
+        id: 4,
+        name: "We love 2 code",
         date: "17 Oct",
         info:
           "Meetup for coders! Join this event to meet new code-loving friends!",
-        image: "codeEvent.jpg"
+        image: "computer-1209641_640.jpg"
       },
-      {
-        id: 2,
-        name: "Meetup with your pet rabbit",
-        date: "30 Sept",
-        info:
-          "Hop toghether with other pet-rabbits and their owners! Carrots are free of charge!",
-        image: "petRabbit.jpg"
-      }
-    ]
+
+    ],
+    listOfAttendedEvents: [2, 4],
+
+
   },
   getters: {
-    listOfEvents (state) {
+    listOfEvents(state) {
       return state.listOfEvents
+    },
+    listOfAttendedEvents(state) {
+      return state.listOfEvents.filter(event => {
+        return state.listOfAttendedEvents.includes(event.id)
+      })
     }
   }
 })
